05217d3615ceed5d28d9a62a028421cd3fcae9d3
nam <nam27062002@gmail.com>
2022-08-25 16:35:45 +0700

    d

--- game.py
+++ game.py
@@ -1,11 +1,13 @@
 from operator import truediv
+from turtle import distance
 import pygame
 pygame.init()
 number = 3
 scaleScreen = [900,500] # scale screen
 scaleFloor = [scaleScreen[0]/3,100] # scale floor
 scaleTubeY = 30
-scalePipe = [20,number * scaleTubeY]
+distanceTube = 35
+scalePipe = [20,(number+1) * scaleTubeY ]
 screen = pygame.display.set_mode((scaleScreen[0],scaleScreen[1])) # create screen
 tube = [] # list tube
 pygame.display.set_caption("Ha noi tower") # set caption
@@ -16,7 +18,7 @@ white = (255,255,255)
 floor = pygame.transform.scale(pygame.image.load("images/floor.png"),(scaleFloor[0],scaleFloor[1]))
 pipe = pygame.transform.scale(pygame.image.load("images/pipe.png"),(scalePipe[0],scalePipe[1]))
 for i in range(7): 
-    tube.append(pygame.transform.scale(pygame.image.load(f"images/{i+1}.png"),(300,scaleTubeY)))
+    tube.append(pygame.transform.scale(pygame.image.load(f"images/{i+1}.png"),(scaleFloor[0] - distanceTube*i,scaleTubeY)))
 #FUNCTION
 #draw graphics
 def drawGraphics():
@@ -26,14 +28,21 @@ def drawGraphics():
     for i in range(3):
         screen.blit(floor,(300*i,scaleScreen[1] - scaleFloor[1]))
         screen.blit(pipe,(yy + 2*i*yy + i*xx,scaleScreen[1] - scaleFloor[1] - scalePipe[1]))
+# check click
+def checkClick(pos):
+    for i in range(number):
+        pass
 # event
 def event():
     global running
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            print()
 def drawTube():
-    screen.blit(tube[0],(0,0))
+    for i in range(number):
+        screen.blit(tube[i],(distanceTube* i/2,scaleScreen[1] - scaleFloor[1] - scaleTubeY*(i+1)))
 while running:
     drawGraphics()
     drawTube()
